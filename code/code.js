593df3ff822619948eac377fc95fb30bb42d1eda
TestCodeChannel <mybeatsone@gmail.com>
2024-05-13 03:47:58 -0500

    Update code.js

--- code/code.js
+++ code/code.js
@@ -1,4 +1,4 @@
-alert("click on an empty space on the page to start");
+alert("click start to start");
 var randomNumber = Math.floor(Math.random()*4);
 var audios = {
     audio1 : new Audio("../sound/ElevenLabs_2024-05-12T01_57_12_Brian_pre_s50_sb75_se0_b_m2.mp3"),
